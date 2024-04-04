aa3ecb869bba119e7b5905c34be30a4184e89400
JuanPabloGHC <jpghc552@hotmail.com>
2024-04-04 01:57:25 -0600

    stats ready

--- web/src/pages/Scala/components/ComponentStat.jsx
+++ web/src/pages/Scala/components/ComponentStat.jsx
@@ -1,8 +1,6 @@
 
 const ComponentStat = (item) => {
 
-    console.log(item)
-
     return (
 
         <div className="max-w-sm p-6 bg-white border border-yellow-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
